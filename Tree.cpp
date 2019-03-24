f618e019830e0e11465483af57981534fdf60ef7
DZand <48325754+DZand@users.noreply.github.com>
2019-03-24 14:41:07 +0200

    Create Tree.cpp

---
+++ Tree.cpp
@@ -0,0 +1,144 @@
+#include "Tree.hpp"
+
+using namespace ariel;
+
+Tree::Tree()
+
+{
+
+    Troot=NULL;
+
+}
+
+Tree::Tree(Node n)
+
+{
+
+    Troot=&n;
+
+}
+
+  int Tree:: size()
+
+    {
+
+    return 0;
+
+    }
+
+    int Tree:: parent(int a)
+
+    {
+
+        return 0;
+
+    }
+
+    int Tree:: left(int a)
+
+    {
+
+        return 0;
+
+    }
+
+    int Tree:: right(int a)
+
+    {
+
+        return 0;
+
+    }
+
+    Node * Tree::insert(int a)
+
+    {
+
+        return Troot;
+
+    }
+
+    int Tree:: root()
+
+    {
+
+        return 0;
+
+    }
+
+    void Tree:: remove (int a)
+
+    {
+
+       
+
+    }
+
+    bool Tree:: contains(int a)
+
+    {
+
+        Node *current = root;	
+
+        while(current->data != data)
+
+        {
+
+	
+
+             if(current != NULL) 
+
+             {	
+
+                    //go to left tree
+
+                    if(current->data > data)
+
+                    {
+
+                         current = current->leftChild;
+
+                    }     //else go to right tree
+
+             else 
+
+             {                
+
+            current = current->rightChild;
+
+         }
+
+			
+
+         //not found
+
+         if(current == NULL)
+
+         {
+
+            return NULL;
+
+         }
+
+      }			
+
+   }
+
+   
+
+   return current;
+
+
+
+        return false;
+
+    }
+
+    void Tree:: print()
+
+    {
+
+   
+
+    }
+
