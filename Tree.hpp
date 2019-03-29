df3e21bdbea5a116d7117e97237262920631493b
DZand <48325754+DZand@users.noreply.github.com>
2019-03-29 15:07:36 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,48 +1,48 @@
+//Benyamin Darmoni , Danielle Zand
 #pragma once
+
 #include <iostream>
 
-namespace ariel
-{
-  struct Node
-  {
+
+namespace ariel{
+   
+  class Node{
+    public:
 	int value;
-	struct Node *left;
-	struct Node *right;
-	Node();
-	Node(int);
-  };
-
-class Tree
-{
-  Node* Troot;
+	Node *left;
+	Node *right;
+                 
+    Node(int);         //constractors
 	
+};
+
+class Tree {
+  
+  Node* Troot;
+
   public:
-
-  Tree(Node);
-
-  Tree();  
-
+  Tree(Node);          //constractors
+  Tree();
+  ~Tree();
     public:          //functions
-
     int size();
-
     int parent(int);
-
     int left(int);
-
     int right(int);
-
-    void insert(int);
-
-    void HelpInsert(Node* main,Node NewNode);
-
+    Node* insert (int);
     int root();
-
     void remove (int);
-
     bool contains(int);
-
     void print();
-
-  };
+    void HelpInsert(Node**,Node**);
+    int HelpParent(Node * ,int );
+   Node* Max(Node*,Node**);
+   void HelpDelete( Node** , int );
+    bool HelpContains(Node*,int);
+    int  HelpSize(Node*);
+    void print2DUtil(Node* , int ); 
+    void HelpMemory(Node* );
+     };
+    
+    
 }
